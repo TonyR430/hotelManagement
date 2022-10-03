d76b7b0950b070cf5fb625399b0050c527464604
Tony Ratley <Tony.ratley430@gmail.com>
2022-10-03 05:11:29 -0500

    Added way to get initial values of each item

--- hotelManagement/hotelManagement.cpp
+++ hotelManagement/hotelManagement.cpp
@@ -12,4 +12,20 @@ int main()
 	int Srooms = 0, Spasta = 0, Sburger = 0, Snoodles = 0, Sshake = 0, Schicken = 0;
 	// Price
 	int Total_rooms = 0, Total_pasta = 0, Total_burger = 0, Total_noodles = 0, Total_shake = 0, Total_Chicken = 0;
+
+	//This prompts the user to get the initial quantity of each
+	cout << "\n \t Quantity of Items We Have";
+	cout << "\n \t Rooms Available";
+	cin >> Qrooms;
+	cout << "\n \t Pasta Available";
+	cin >> Qpasta;
+	cout << "\n \t Burgers Available";
+	cin >> Qburger;
+	cout << "\n \t Noodles Available";
+	cin >> Qnoodles;
+	cout << "\n \t Shakes Available";
+	cin >> Qshake;
+	cout << "\n \t Chicken Available";
+	cin >> Qchicken;
+
 }
