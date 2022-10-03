59b7c8b7dd8f6eb75609141c376e7d6992eeb1bd
Tonyr <Tonyr@TONYDESK>
2022-10-03 05:00:30 -0500

    Add project files.

---
+++ hotelManagement/hotelManagement.cpp
@@ -0,0 +1,15 @@
+//Hotel Management Program from Youtube to get back into practice
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int quant;
+	int choice;
+	//How many of each we have
+	int Qrooms = 0, Qpasta = 0, Qburger = 0, Qnoodles = 0, Qshake = 0, Qchicken = 0;
+	//How many we have sold
+	int Srooms = 0, Spasta = 0, Sburger = 0, Snoodles = 0, Sshake = 0, Schicken = 0;
+	// Price
+	int Total_rooms = 0, Total_pasta = 0, Total_burger = 0, Total_noodles = 0, Total_shake = 0, Total_Chicken = 0;
+}
