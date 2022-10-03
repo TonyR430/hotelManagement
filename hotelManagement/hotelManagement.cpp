e1a1100800fa0e4fa55b0fb0dc9b76c9c491cac5
Tony Ratley <Tony.ratley430@gmail.com>
2022-10-03 05:50:16 -0500

    Added Menu for the customer to select from and started the swith statement to handle the choice

--- hotelManagement/hotelManagement.cpp
+++ hotelManagement/hotelManagement.cpp
@@ -15,17 +15,51 @@ int main()
 
 	//This prompts the user to get the initial quantity of each
 	cout << "\n \t Quantity of Items We Have";
-	cout << "\n \t Rooms Available";
+	cout << "\n \t Rooms Available: ";
 	cin >> Qrooms;
-	cout << "\n \t Pasta Available";
+	cout << "\n \t Pasta Available: ";
 	cin >> Qpasta;
-	cout << "\n \t Burgers Available";
+	cout << "\n \t Burgers Available: ";
 	cin >> Qburger;
-	cout << "\n \t Noodles Available";
+	cout << "\n \t Noodles Available: ";
 	cin >> Qnoodles;
-	cout << "\n \t Shakes Available";
+	cout << "\n \t Shakes Available: ";
 	cin >> Qshake;
-	cout << "\n \t Chicken Available";
+	cout << "\n \t Chicken Available: ";
 	cin >> Qchicken;
+
+	//This allows the customer to select what they wish to order
+	cout << "\n \t \t \t Please Select From The Menu Options: ";
+	cout << "\n \n 1)Rooms";
+	cout << "\n 2)Pasta";
+	cout << "\n 3)Burger";
+	cout << "\n 4)Noodles";
+	cout << "\n 5)Shake";
+	cout << "\n 6)Chicken";
+	cout << "\n 7)Information Regarding Sales and Collection";
+	cout << "\n 8)Exit";
+
+	cout << "\n\n Please enter your choice: ";
+	cin >> choice;
+
+	//This handles the choice that the customer makes
+	switch (choice)
+	{
+		case 1:
+			cout << "Enter Number of Rooms Requested: ";
+			cin >> quant;
+			if (Qrooms - Srooms >= quant)
+			{
+				Srooms = Srooms + quant;
+				Total_rooms = Total_rooms + quant * 1200;
+				cout << "\n \n \t \t" << quant << " room/rooms have been assigned to you."
+			}
+			else
+			{
+				cout << "\n \t Only " << Qrooms - Srooms << " rooms are available at this time";
+				break;
+			}
+
+	}
 
 }
